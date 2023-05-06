61ad39b38ed05380bf075e8624714cbcfdca3d1e
Nghiem Hai <nghiemqhai1009@gmail.com>
2023-05-06 15:35:18 +0700

    add file read

---
+++ BTTH01/php/read_file.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quản lý sinh viên</title>
+</head>
+<body>
+	<h1>QUẢN LÝ SINH VIÊN</h1>
+	<table>
+		<tr>
+            <th>Mã sinh viên</th>
+			<th>Họ và tên</th>
+			<th>Tuổi</th>
+		</tr>
+		<?php
+			$file = fopen("../dssv.txt", "r");
+			$dataList = array();
+			while (!feof($file)) {
+				$line = fgets($file);
+				$data = explode(",", $line);
+				array_push($dataList, $data);
+			}
+			fclose($file);
+
+			foreach ($dataList as $data) {
+				echo "<tr><td>" . $data[0] . "</td><td>" . $data[1] . "</td><td>" . $data[2] ."</td></tr>";
+			}
+		?>
+	</table>
+</body>
+</html>
